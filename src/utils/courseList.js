417512c54618050985a0c4de3bb930db76eed919
priyanshrd <priyansh17rd@gmail.com>
2025-07-14 20:45:06 +0530

    Added IPRE materials

--- src/utils/courseList.js
+++ src/utils/courseList.js
@@ -12,8 +12,10 @@ const courseList = {
 
   ECE: [
     { code: "HS261TA", title: "Principles of Management & Economics" },
+    { code: "EC352IA", title: "Digital VLSI Design" },
     { code: "EC362IA", title: "SystemVerilog for Design and Verification" },
     { code: "EC363IA", title: "Computer Networks and Protocols" },
+    { code: "EC354TA", title: "Embedded System Design" },
     { code: "EC364TA", title: "Digital Signal Processing with ML" },
 
     // Core Electives (Group D)
@@ -23,13 +25,35 @@ const courseList = {
     { code: "EC365TD4", title: "Digital CMOS Design" }
   ],
 
-  Institutional: [
+  EIE: [
+    { code: "HS351TA", title: "Entrepreneurship and IPR" },
+    { code: "EC352IA", title: "Digital VLSI Design" }, // Shared with EC
+    { code: "EI253IA", title: "Automatic Process Control & Virtual Instrumentation" },
+    { code: "EC354TA", title: "Embedded System Design" }, // Shared with EC
+    { code: "EI255TBA", title: "Microelectromechanical Systems & Applications" },
+    { code: "EI255TBB", title: "Safety Automation for Industries" },
+    { code: "EI255TBC", title: "Product Design Technology" },
+    { code: "EI255TBD", title: "Biopotentials & Medical Devices" },
+    { code: "EI256TCA", title: "Cloud Computing and Distributed Systems" },
+    { code: "EI256TCB", title: "Fuzzy Logic and Neural Network" },
+    { code: "EI256TCC", title: "User-centric Computing for HCI" },
+    { code: "EI256TCD", title: "Mechatronics" },
+    { code: "EI256TCE", title: "VLSI Signal Processing" },
+    { code: "EI256TCF", title: "Health Research and Fundamentals" },
+    { code: "HS261TA", title: "Principles of Management & Economics" }, // Shared with EC
+    { code: "EI362IA", title: "Industrial Automation Technologies" },
+    { code: "EI363IA", title: "Data Communication Networks" },
+    { code: "EI265TDA", title: "Industrial Wireless Technologies" },
+    { code: "EI265TDB", title: "Virtual and Augmented Reality" },
+    { code: "EI265TDC", title: "Data Analytics" },
+    { code: "EI265TDD", title: "System on Chip" }
+  ],
 
-    // Common Electives
+  Institutional: [
     { code: "CS266TED", title: "Robotic Process Automation (RPA)" },
     { code: "EC266TEH", title: "Human Machine Interface (HMI)" },
     { code: "MA266TEU", title: "Mathematical Modelling" },
-    { code: "CS365TDZ", title: "Data Structures and Algorithms" },
+    { code: "CS365TDZ", title: "Data Structures and Algorithms" }
   ]
 };
 
